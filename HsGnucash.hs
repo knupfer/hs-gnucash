171acf5728c724d73f71893470e43b038f801986
knupfer <fknupfer@gmail.com>
2015-04-19 17:48:43 +0200

    parse date with attoparsec

--- HsGnucash.hs
+++ HsGnucash.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import qualified Data.Attoparsec.Text as A
 import qualified Data.Text as T
 import Data.List
 import System.Environment
@@ -8,9 +9,9 @@ import Control.Arrow
 import Text.XML.HXT.Core
 
 data Date = Date Int Int Int deriving (Eq, Show, Ord)
-data Cent = Cent Int
+data Cent = Cent Int deriving (Eq, Show, Ord)
 data Account = Asset | Receivable | Liability | Payable | Income | Expense | Equity | Bank
-data Transaction = Transaction Date Int String deriving (Eq, Show, Ord)
+data Transaction = Transaction Date Cent String deriving (Eq, Show, Ord)
 
 main :: IO ()
 main = do
@@ -24,5 +25,10 @@ getTransactions = deep $ hasName "gnc:transaction"
             >>> (deep (hasName "trn:date-posted" /> hasName "ts:date")
                 &&& (deep (hasName "split:value") &&& deep (hasName "split:account")))
             >>> deep getText *** deep getText *** deep getText
-            >>> arr (\(a, (b,c)) -> Transaction (Date 1 2 3) (money b) c)
-  where money = read . takeWhile (/= '/')
+            >>> arr (\(a, (b,c)) -> Transaction (date' $ T.pack a) (money b) c)
+  where money = Cent . read . takeWhile (/= '/')
+        date' = either error id . A.parseOnly (do
+              y <- A.decimal <* A.char '-'
+              m <- A.decimal <* A.char '-'
+              d <- A.decimal
+              return (Date y m d))
